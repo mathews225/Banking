45304d37b776c589ef788a0f20ec9307a6258275
mathews225 <petemathews14@gmail.com>
2021-02-16 09:18:31 -0500

    Add project files.

---
+++ Banking/Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking {
+    class Account {
+
+        public string AcctNbr { get; set; }
+        public string RoutNbr { get; set; }
+        public string Description { get; set; }
+        public double Balance { get; set; } = 0;
+
+        public void Deposit(double amount) {
+            if (amount > 0) {
+                Balance += amount;
+                Console.WriteLine($"${amount} Deposit");
+                Console.WriteLine($"${Balance} Balance\n");
+            }
+            else {
+                Console.WriteLine("Amount must be greater than $0. Did you want to withdraw instead?");
+            };
+        }
+
+        public void Withdraw(double amount) {
+                if (amount > 0) {
+                    if (amount > Balance) {
+                        Console.WriteLine($"Insufficient Funds\n");
+                    }
+                    else {
+                        Balance -= amount;
+                        Console.WriteLine($"${amount} Withdraw");
+                        Console.WriteLine($"${Balance} Balance\n");
+                    }
+                } else {
+                    Console.WriteLine("Amount must be greater than $0. Did you want to deposit instead?");
+                }
+        }
+
+
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Banking {
+    class Program {
+        static void Main(string[] args) {
+            var acct1 = new Account();
+            acct1.AcctNbr = "100";
+            acct1.Deposit(200);
+            acct1.Withdraw(30);
+            acct1.Withdraw(-200);
+            Console.WriteLine($"Current account balance is ${acct1.Balance}.");
+
+
+        }
+    }
+}
