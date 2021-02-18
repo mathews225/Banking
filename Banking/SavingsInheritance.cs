a2c27b161f830b6ed36eaa4e5116a215340791e0
mathews225 <petemathews14@gmail.com>
2021-02-18 09:20:11 -0500

    savings inher constructor

---
+++ Banking/SavingsInheritance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking {
+	class SavingsInheritance : Account {
+
+		public SavingsInheritance() : base() { }
+
+		public SavingsInheritance(string AcctNbr, string Desc) : base(AcctNbr, Desc) { }
+
+
+
+
+	}
+}
