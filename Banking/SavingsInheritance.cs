9a88b48245db63980fd944cb2a9284a1a60c43c3
mathews225 <petemathews14@gmail.com>
2021-02-18 09:26:26 -0500

    savings inher interest rate

--- Banking/SavingsInheritance.cs
+++ Banking/SavingsInheritance.cs
@@ -5,12 +5,15 @@ using System.Text;
 namespace Banking {
 	class SavingsInheritance : Account {
 
+		public double InterestRate { get; set; } = 0.12;
+
 		public SavingsInheritance() : base() { }
 
 		public SavingsInheritance(string AcctNbr, string Desc) : base(AcctNbr, Desc) { }
 
-
-
+		public SavingsInheritance(string AcctNbr, double IntRate) : base (AcctNbr, "New Savings Account") {
+			this.InterestRate = IntRate;
+		}
 
 	}
 }
